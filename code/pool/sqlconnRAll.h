ef7e0d8cf9d2ffff12edfcea87ce567b937f84ee
dusens <102510869@qq.com>
2022-08-20 21:05:56 +0800

    <feat><webserver>mysql连接池的分配

--- code/pool/sqlconnRAll.h
+++ code/pool/sqlconnRAll.h
@@ -6,5 +6,22 @@
 #define WEBSERVER_SQLCONNRALL_H
 
 
+#include "sqlconnpool.h"
+
+class SqlConnRAII {
+
+public:
+    SqlConnRAII(MYSQL **sql, SqlConnPool *connpool) {
+        assert(connpool);
+        *sql = connpool->GetConn();
+        sql_ = *sql;
+        connpool_ = connpool;
+    }
+
+private:
+    MYSQL *sql_;
+    SqlConnPool *connpool_;
+};
+
 
 #endif //WEBSERVER_SQLCONNRALL_H
--- code/pool/sqlconnpool.h
+++ code/pool/sqlconnpool.h
@@ -6,5 +6,40 @@
 #define WEBSERVER_SQLCONNPOOL_H
 
 #include <mysql/mysql.h>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include <thread>
+#include <cassert>
+
+class SqlConnPool {
+public:
+    static SqlConnPool *Instance();
+
+    MYSQL *GetConn();
+
+    void FreeConn(MYSQL *conn);
+
+    int GetFreeConnCount();
+
+    void Init(const char *host, int port, const char *user, const char *pwd,
+              const char *dbName, int connSize);
+
+    void ClosePool();
+
+private:
+    SqlConnPool();
+
+    ~SqlConnPool();
+
+    int MAX_CONN_;
+    int userCount_;
+    int freeCount_;
+
+    std::queue<MYSQL *> connQue_;
+    std::mutex mtx_;
+    sem_t semId_;
+};
 
 #endif //WEBSERVER_SQLCONNPOOL_H
